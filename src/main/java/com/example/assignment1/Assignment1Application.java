2bdc05c3c6b6149ca5f75f4c87d2a6b049effd03
Parvathi <pava@uw.edu>
2020-10-10 10:47:10 -0700

    homework1

---
+++ src/main/java/com/example/assignment1/Assignment1Application.java
@@ -0,0 +1,11 @@
+package com.example.assignment1;
+import uw.syp.java.tools.Turtle;
+
+public class Assignment1Application {
+
+	public static void main(String[] args) {
+			Turtle toots=new Turtle();
+			toots.fillCircle( 64 );
+	}
+
+}
---
+++ src/test/java/com/example/assignment1/Assignment1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.assignment1;
+
+import org.junit.jupiter.api.Test;
+import uw.syp.java.tools.Turtle;
+
+class Assignment1ApplicationTests {
+
+	@Test
+	void test() {
+		Turtle toots   = new Turtle();
+		toots.fillCircle( 64 );
+		Turtle.sleep( 2000 );
+
+	}
+
+}
